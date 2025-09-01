d13385c217620ee7c77c33d3de98160ed0e527bb
linyueCS-IT <6197673@johnabbottcollege.net>
2025-09-01 10:16:33 -0400

    Implement game play with play1 and play2

--- pong/Ball.js
+++ pong/Ball.js
@@ -9,7 +9,7 @@ export default class Ball {
    * @param {Number} width The ball's width.
    * @param {Number} height The ball's height.
    */
-  constructor(x, y, width, height) {
+  constructor(x, y, width, height, canvasHeight, canvasWidth) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -19,7 +19,6 @@ export default class Ball {
     this.dx = 200;
     this.dy = 200;
   }
-
   /**
    * Position the ball at the specified coordinates
    * and generate a new random velocity.
@@ -35,20 +34,63 @@ export default class Ball {
   }
 
   /**
+   * Expects a paddle as an argument and returns true or false,
+   * depending on whether their rectangles overlap.
+   *
+   * @param {Object} paddle
+   * @returns Whether or not there was a collision.
+   */
+  didCollide(paddle) {
+    /**
+     * First, check to see if the left edge of either is farther
+     * to the right than the right edge of the other. Then check
+     * to see if the bottom edge of either is higher than the top
+     * edge of the other.
+     */
+    if (
+      this.x < paddle.x + paddle.width &&
+      paddle.x < this.x + this.width &&
+      this.y < paddle.y + paddle.height &&
+      paddle.y < this.y + this.height
+    ) {
+      return true; // We have a collision!
+    }
+    // If the above aren't true, they're overlapping.
+    return false;
+  }
+  /**
    * Update our paddle based on its DX and DY only if we're in play state;
    * scale the velocity by dt so movement is framerate-independent.
    *
-   * @param {Number} dt Time since the last frame.
+   * @param {Number} deltaTime Time since the last frame.
+   * @param {Object} player1
+   * @param {Object} player2
    */
-  update(dt) {
-    this.x += this.dx * dt;
-    this.y += this.dy * dt;
-  }
+  update(deltaTime, player1, player2) {
+    /**
+     * Detect ball collision with paddles, reversing dx if true and
+     * slightly increasing it, then altering the dy based on the position
+     * of collision
+     */
+    if (this.didCollide(player1) || this.didCollide(player2)) {
+      this.dx *= -1;
+    }
 
+    // Detect upper and lower screen boundary collision and reverse if collided.
+    if (this.y <= 0) {
+      this.y = 0;
+      this.dy *= -1;
+    }
+    // -this.height to account for the ball's size.
+    if (this.y >= this.canvasHeight - 20) {
+      this.y = this.canvasHeight - 20;
+      this.dy *= -1;
+    }
+    this.x += this.dx * deltaTime;
+    this.y += this.dy * deltaTime;
+  }
   /**
    * Draw the ball to the screen.
-   *
-   * @param {CanvasRenderingContext2D} context
    */
   render(context) {
     context.fillRect(this.x, this.y, this.width, this.height);
--- pong/main.js
+++ pong/main.js
@@ -1,3 +1,6 @@
+import Paddle from "./Paddle.js";
+import Ball from "./Ball.js";
+
 /**
  * We initialize our game by grabbing the `canvas` element from
  * the DOM located in `index.html` and getting the `context` object
@@ -11,33 +14,52 @@ const CANVAS_HEIGHT = 720;
 // Set the dimensions of the play area.
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
-canvas.setAttribute("tabindex", 1); // Allows the canvas to receive user input. // Allows the canvas to receive user input.
+canvas.setAttribute("tabindex", 1); // Allows the canvas to receive user input.
 
 // Now that the canvas element has been prepared, we can add it to the DOM.
 document.body.appendChild(canvas);
 
-// This will be used to calculate delta time in `gameLoop()`.
-let lastTime = 0;
-
 // Initialize score variables for rendering on the screen and keeping track of the winner.
 let player1Score = 0;
 let player2Score = 0;
+
+// Initialize the player paddles and the ball as class instances.
+const player1 = new Paddle(30, 30, 20, 200, CANVAS_HEIGHT);
+const player2 = new Paddle(
+  CANVAS_WIDTH - 50,
+  CANVAS_HEIGHT - 230,
+  20,
+  200,
+  CANVAS_HEIGHT
+);
+const ball = new Ball(
+  CANVAS_WIDTH / 2 - 10,
+  CANVAS_HEIGHT / 2 - 10,
+  20,
+  20,
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH
+);
+
+/**
+ * Game state variable used to transition between different parts of the game.
+ * Used for beginning, menus, main game, high score list, etc.
+ * We will use this to determine behavior during `render()` and `update()`.
+ */
+let gameState = "start";
+
+const sounds = {
+  score: new Audio("./score.wav"),
+};
+
+// Keep track of what keys were pressed/unpressed.
+const keys = {};
 
 // Load a custom font to use.
 const myFont = new FontFace("Bytesized", "url(./Bytesized-Regular.ttf)");
 myFont.load().then((font) => {
   document.fonts.add(font);
 });
-
-// Paddle positions on the Y axis (they can only move up or down).
-let player1Y = 30;
-let player2Y = CANVAS_HEIGHT - 230;
-
-// Speed at which we will move our paddle; multiplied by `dt` in `update()`.
-const PADDLE_SPEED = 1000;
-
-// Keep track of what keys were pressed/unpressed.
-const keys = {};
 
 // Set the appropriate key in our `keys` object to `true` if a key was pressed.
 canvas.addEventListener("keydown", (event) => {
@@ -49,13 +71,17 @@ canvas.addEventListener("keyup", (event) => {
   keys[event.key] = false;
 });
 
+// This will be used to calculate delta time in `gameLoop()`.
+let lastTime = 0;
+
 /**
  * This function is the heartbeat of the application. It is called
  * 60 times per second (depending on your monitor's refresh rate) and
  * it is what we will use to drive our game's animations. The way
  * that this function is called 60 times per second is by using JavaScript's
  * `requestAnimationFrame()` API.
- * @param {Number} currentTime currentTime How much time has elapsed since the age loaded.
+ *
+ * @param {Number} currentTime How much time has elapsed since the page loaded.
  */
 function gameLoop(currentTime = 0) {
   // Calculates delta time and converts it to seconds instead of milliseconds.
@@ -71,39 +97,79 @@ function gameLoop(currentTime = 0) {
  * `dt` (i.e., DeltaTime) will be the elapsed time in seconds since the last
  * frame, and we can use this to scale any changes in our game for even behavior
  * across frame rates. This is where the logic of our game will be executed.
- * @param  {Number} deltaTime How much time has elapsed since the last time this was called.
+ *
+ * @param {Number} deltaTime How much time has elapsed since the last time this was called.
  */
 function update(deltaTime) {
+  // If the enter key was pressed...
+  if (keys.Enter) {
+    /**
+     * We have to immediately set the enter field of the keys object to false
+     * because technically, this field will be true for the entire time the key
+     * is pressed. If you hold down the enter key, you don't want the ball to
+     * repeatedly launch over and over again.
+     */
+    keys.Enter = false;
 
+    /**
+     * If we press enter during the start state of the game, we'll go into the
+     * play state. During play state, the ball will move in a random direction.
+     */
+    if (gameState === "start") {
+      gameState = "play";
+    } else {
+      gameState = "start";
+
+      ball.reset(CANVAS_WIDTH / 2 - 10, CANVAS_HEIGHT / 2 - 10);
+    }
+  }
+
+  if (gameState === "play") {
+    ball.update(deltaTime, player1, player2);
+  }
+
+  // if ball goes off the left or right edge increment
+  if (ball.x < 0) {
+    sounds.score.play();
+    player2Score++;
+    ball.reset(CANVAS_WIDTH / 2 - 10, CANVAS_HEIGHT / 2 - 10);
+    gameState = "start";
+  }
+  if (ball.x > CANVAS_WIDTH - 20) {
+    sounds.score.play();
+    player1Score++;
+    ball.reset(CANVAS_WIDTH / 2 - 10, CANVAS_HEIGHT / 2 - 10);
+    gameState = "start";
+  }
+
+  player1.update(deltaTime);
+  player2.update(deltaTime);
+
+  // Player1 move
   if (keys.w) {
-    // player1Y -= PADDLE_SPEED * dt;
-    player1Y = Math.max(0, player1Y - PADDLE_SPEED * deltaTime);
+    player1.moveUp();
+  } else if (keys.s) {
+    player1.moveDown();
+  } else {
+    player1.stop();
   }
-
-  if (keys.s) {
-    // player1Y += PADDLE_SPEED * dt
-    player1Y = Math.min(
-      CANVAS_HEIGHT - 200,
-      player1Y + PADDLE_SPEED * deltaTime
-    );
-  }
-
+  // Player2 move
   if (keys.ArrowUp) {
-    // player2Y -= PADDLE_SPEED * dt;
-    player2Y = Math.max(0, player2Y - PADDLE_SPEED * deltaTime);
-  }
-
-  if (keys.ArrowDown) {
-    // player2Y += PADDLE_SPEED * dt
-    player2Y = Math.min(
-      CANVAS_HEIGHT - 200,
-      player2Y + PADDLE_SPEED * deltaTime
-    );
+    player2.moveUp();
+  } else if (keys.ArrowDown) {
+    player2.moveDown();
+  } else {
+    player2.stop();
   }
 
   render();
 }
 
+/**
+ * This function is also executed at each frame since it is called by
+ * `update()`. It is called after the update step completes so that we
+ * can draw things to the screen once they've changed.
+ */
 function render() {
   /**
    * Erase whatever was previously on the canvas so that we can start
@@ -117,23 +183,20 @@ function render() {
   context.textBaseline = "middle";
   context.textAlign = "center";
 
-  /**
-   * The paddles are simply rectangles we draw on the screen at certain
-   * points, as is the ball.
-   */
-
   // Render scores at the top of the screen.
   context.fillText(`${player1Score}`, CANVAS_WIDTH * 0.25, 75);
   context.fillText(`${player2Score}`, CANVAS_WIDTH * 0.75, 75);
-  // Left paddle
-  context.fillRect(30, player1Y, 20, 200); // x, y, width, height
 
-  // Ball
-  context.fillRect(CANVAS_WIDTH * 0.5 - 10, CANVAS_HEIGHT * 0.5 - 10, 20, 20);
+  // Render the ball using its class' render method.
+  ball.render(context);
 
-  // Right paddle
-  context.fillRect(CANVAS_WIDTH - 50, player2Y, 20, 200);
+  // Render paddles using their class' render method.
+  player1.render(context);
+  player2.render(context); // Right paddle
 }
 
+// Start the game loop.
 gameLoop();
+
+// Focus the canvas so that user doesn't have to click on it.
 canvas.focus();
